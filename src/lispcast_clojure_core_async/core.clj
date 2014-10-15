6ee6d67d3f752392f680662e1b507dda02b58644
Eric Normand <ericwnormand@gmail.com>
2014-10-15 00:03:12 -0500

    Instrument build-car with printlns.

--- src/lispcast_clojure_core_async/core.clj
+++ src/lispcast_clojure_core_async/core.clj
@@ -5,24 +5,32 @@
    [clojure.core.async :as async
     :refer [>! <! alts! chan put! go]]))
 
-(defn build-car []
+(defn build-car [n]
+  (println n "Starting build")
   (let [body (loop []
                (let [part (take-part)]
                  (if (body? part)
                    part
                    (recur))))
+        _ (println n "Got body")
         wheel1 (loop []
                  (let [part (take-part)]
                    (if (wheel? part)
                      part
                      (recur))))
+        _ (println n "Got first wheel")
         wheel2 (loop []
                  (let [part (take-part)]
                    (if (wheel? part)
                      part
                      (recur))))
+        _ (println n "Got second wheel")
         bw (attach-wheel body wheel1)
+        _ (println n "Attached first wheel")
         bww (attach-wheel bw wheel2)
+        _ (println n "Attached second wheel")
         box (box-up bww)]
-    (put-in-truck box)))
+    (println n "Boxed up car")
+    (put-in-truck box)
+    (println n "Done!!")))
 
