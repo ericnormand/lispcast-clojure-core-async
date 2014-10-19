5d0ce4017fbd98582866f5eed50ff089fd6c7980
Eric Normand <ericwnormand@gmail.com>
2014-10-18 22:47:09 -0500

    Add first assembly line.

--- src/lispcast_clojure_core_async/core.clj
+++ src/lispcast_clojure_core_async/core.clj
@@ -43,3 +43,67 @@
   (dotimes [x 3]
     (go
       (time (println (take-part))))))
+
+(defn assembly-line []
+  (let [body-chan (chan)
+        wheel1-chan (chan)
+        wheel2-chan (chan)
+        body+wheel-chan (chan)
+        body+2-wheels-chan (chan)
+        box-chan (chan)]
+    (go
+      (while true
+        (let [body (loop []
+                     (let [part (take-part)]
+                       (if (body? part)
+                         part
+                         (recur))))]
+          (println "Got body")
+          (>! body-chan body))))
+    (go
+      (while true
+        (let [wheel1 (loop []
+                       (let [part (take-part)]
+                         (if (wheel? part)
+                           part
+                           (recur))))]
+          (println "Got first wheel")
+          (>! wheel1-chan wheel1))))
+    (go
+      (while true
+        (let [wheel2 (loop []
+                       (let [part (take-part)]
+                         (if (wheel? part)
+                           part
+                           (recur))))]
+          (println "Got second wheel")
+          (>! wheel2-chan wheel2))))
+
+    (go
+      (while true
+        (let [body (<! body-chan)
+              wheel1 (<! wheel1-chan)
+              bw (attach-wheel body wheel1)]
+          (println "Attached first wheel")
+          (>! body+wheel-chan bw))))
+
+    (go
+      (while true
+        (let [bw (<! body+wheel-chan)
+              wheel2 (<! wheel2-chan)
+              bww (attach-wheel bw wheel2)]
+          (println "Attached second wheel")
+          (>! body+2-wheels-chan bww))))
+
+    (go
+      (while true
+        (let [box (box-up (<! body+2-wheels-chan))]
+          (println "Boxed up car")
+          (>! box-chan box))))
+
+    (go
+      (time
+       (dotimes [x 10]
+         (time
+          (put-in-truck (<! box-chan)))
+         (println "Done!!"))))))
