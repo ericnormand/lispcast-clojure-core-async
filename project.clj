2b6795930e0635640582c434e6114cdb89b744de
Eric Normand <ericwnormand@gmail.com>
2020-04-20 15:14:05 -0500

    Use Public Domain license.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject lispcast-clojure-core-async "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+  :description "Part of the Clojure core.async course on PurelyFunctional.tv"
+  :url "https://purelyfunctional.tv/courses/clojure-core-async/"
+  :license {:name "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
+            :url "http://creativecommons.org/publicdomain/zero/1.0/"}
   :repl-options {:init-ns lispcast-clojure-core-async.core})
