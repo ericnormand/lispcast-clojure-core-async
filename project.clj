99fe593cc3b0646a33e045050d3409127e5e4502
Eric Normand <ericwnormand@gmail.com>
2020-04-20 15:14:22 -0500

    Update Clojure and core.async deps

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject lispcast-clojure-core-async "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :description "Part of the Clojure core.async course on PurelyFunctional.tv"
   :url "https://purelyfunctional.tv/courses/clojure-core-async/"
   :license {:name "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.async "1.1.587"]]
   :repl-options {:init-ns lispcast-clojure-core-async.core})
