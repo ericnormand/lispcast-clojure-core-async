e9e900ef88e6bc43ca20556ba0276d923deaf014
Eric Normand <ericwnormand@gmail.com>
2020-04-21 08:21:02 -0500

    Update LICENSE, dependencies, README

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "1.1.587"]]
-  :repl-options {:init-ns lispcast-clojure-core-async.core})
+  :repl-options {:init-ns lispcast-clojure-core-async.core}
+  :jvm-opts ["-Dclojure.core.async.pool-size=1000"])
