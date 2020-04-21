f13b3153112d029210b977b5528231709d8e734b
Eric Normand <ericwnormand@gmail.com>
2020-04-21 08:19:57 -0500

    Update LICENSE, dependencies, README

--- test/lispcast_clojure_core_async/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns lispcast-clojure-core-async.core-test
-  (:require [clojure.test :refer :all]
-            [lispcast-clojure-core-async.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
